b9346a07e428e757ef1c5f564867d91e14a7c6d0
ErmekSolpuev <ermeksee27@gmail.com>
2022-07-30 12:33:33 +0600

    Создал модель Опроса (Poll) со следующими полями: Вопрос - текстовое, обязательное. Дата и время создания опроса - автозаполнение.
    Создал модель Варианта ответа (Choice) со следующими полями:
    
    Текст варианта - текстовое, обязательное.
    Опрос - внешний ключ, если удаляется опрос - его варианты ответа тоже удаляются.

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Poll, Choice
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question', 'created_time']
+    list_display_links = ['question']
+    list_filter = ['question']
+    search_fields = ['question']
+    fields = ['question', 'created_time', 'updated_time']
+    readonly_fields = ['created_time', 'updated_time']
+
+# admin.site.register(Tag)
+admin.site.register(Choice)
+admin.site.register(Poll, PollAdmin)
